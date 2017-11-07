5dc3d17b6c030159d6310ecba55087b3a46c65fc
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-07 12:49:38 -0200

    Listing customers

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Odin\Customer;
 
 Route::get('/', function () {
-    return view('welcome');
+
+//    LIST CUSTOMERS
+//    $customers = Odin\Customer::paginate(10);
+//    dd($customers->toArray());
+//    echo $customers->render();
+
 });
