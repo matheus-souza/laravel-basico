aea39ca7b408bab05bdce24c5e0c0e4d815b15de
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-08 12:39:23 -0200

    Field fill added

--- app/Customer.php
+++ app/Customer.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+    protected $fillable = ['name', 'birthdate', 'special_customer', 'city', 'state'];
+
     protected $casts = [
         'special_customer' => 'boolean',
         'birthdate' => 'date',
