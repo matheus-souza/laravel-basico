fa223abfa0650942bc542be547a376a79d7e1c01
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-09 12:59:26 -0200

    Clean route file

--- routes/web.php
+++ routes/web.php
@@ -3,37 +3,5 @@
 use Odin\Customer;
 
 Route::get('/', function () {
-
-//    LIST CUSTOMERS
-//    $customers = Odin\Customer::paginate(10);
-//    dd($customers->toArray());
-//    echo $customers->render();
-
-//    NEW CUSTOMER
-    $data = [
-        'name' => 'Matheus Souza',
-        'city' => 'Venâncio Aires',
-        'state' => 'RS',
-        'special_customer' => true,
-        'birthdate' => '1998-02-11 00:00:00'
-    ];
-    $customer = Customer::create($data);
-    dd($customer->toArray());
-
-//    GET CUSTOMER
-//    $customer = Customer::find(51);
-//    dd($customer->birthdate);
-
-//    UPDATE CUSTOMER
-//    $data = [
-//        'name' => 'Nome novo',
-//        'birthdate' => '1998-02-12 00:00:00',
-//    ];
-//
-//    $customer = Customer::find(51);
-//    $customer->name = 'Novo nome';
-//    SAVE METHOD
-//    $customer->save();
-//    UPDATE METHOD
-//    $customer->update($data);
+    return 'Rota principal no Laravel';
 });
