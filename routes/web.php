4daa8cc04c807c7aef89dec36c55f00a359e75fc
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-09 13:02:04 -0200

    Creating client routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Odin\Customer;
 Route::get('/', function () {
     return 'Rota principal no Laravel';
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('clientes', ['as' => 'customer.index', 'uses' => 'CustomersController@index']);
+    Route::get('cliente/{id}', ['as' => 'customer.get', 'uses' => 'CustomersController@get']);
+    Route::post('cliente', ['as' => 'customer.create', 'uses' => 'CustomersController@create']);
+    Route::put('cliente/{id}', ['as' => 'customer.update', 'uses' => 'CustomersController@update']);
+    Route::delete('cliente/{id}', ['as' => 'customer.delete', 'uses' => 'CustomersController@delete']);
+});
