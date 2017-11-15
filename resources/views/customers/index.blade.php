054e0ee4bbe4150867c4d820d82dc51c45025aa6
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-14 22:01:02 -0200

    Added first view

---
+++ resources/views/customers/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel Básico</title>
+</head>
+<body>
+    <h1>Bem vindo ao mundo Laravel!</h1>
+    <?php
+        var_dump($customers);
+    ?>
+</body>
+</html>
