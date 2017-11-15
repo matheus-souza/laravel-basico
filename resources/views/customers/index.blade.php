43f44b67431c876e637e7f87af4638306be37fd5
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-14 22:03:07 -0200

    Customers listing added in view

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -5,9 +5,32 @@
     <title>Laravel Básico</title>
 </head>
 <body>
-    <h1>Bem vindo ao mundo Laravel!</h1>
-    <?php
-        var_dump($customers);
-    ?>
+    <div class="container">
+        <h1>Bem vindo ao mundo Laravel!</h1>
+
+        <table class="table table-bordered table-striped">
+            <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Cidade/Estado</th>
+                <th>Nascimento</th>
+                <th>Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach($customers as $customer)
+                <tr>
+                    <td>{{ $customer->name }}</td>
+                    <td>{{ $customer->cyte }} {{ $customer->state }}</td>
+                    <td>{{ $customer->birthdate }}</td>
+                    <td width="1%" nowrap="">
+                        <a href="" class="btn btn-xs btn-primary">Editar</a>
+                        <a href="" class="btn btn-xs btn-primary">Excluir</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
