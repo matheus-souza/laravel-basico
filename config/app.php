49f4c48ef2e9e0a237ed747f22134d405672037a
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-16 12:28:02 -0200

    Added LaravelCollective in project

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         // Odin\Providers\BroadcastServiceProvider::class,
         Odin\Providers\EventServiceProvider::class,
         Odin\Providers\RouteServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -225,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
